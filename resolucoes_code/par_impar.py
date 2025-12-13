3e0c6e21fa03aafc2c02143f8584d1342835ada5
Jeferson Silva <jefinho_m_@outlook.com>
2025-12-13 23:35:49 +0000

    Atualiza solução par ou ímpar

--- resolucoes_code/par_impar.py
+++ resolucoes_code/par_impar.py
@@ -1,9 +1,20 @@
 def verificar_par_ou_impar():
-    numero = int(input("Digite um número inteiro: "))
-    if numero % 2 == 0:
-        print(f"O número {numero} é par.")
+    numero_par = int(input("Digite um número para verificar se é PAR: "))
+    numero_impar = int(input("Digite um número para verificar se é ÍMPAR: "))
+
+    # Verifica PAR
+    if numero_par % 2 == 0:
+        print(f"O número {numero_par} é par.")
     else:
-        print(f"O número {numero} é ímpar.")    
+        print(f"O número {numero_par} NÃO é par.")
+
+    # Verifica ÍMPAR
+    if numero_impar % 2 != 0:
+        print(f"O número {numero_impar} é ímpar.")
+    else:
+        print(f"O número {numero_impar} NÃO é ímpar.")
+
 
 if __name__ == "__main__":
-    verificar_par_ou_impar()    
+    verificar_par_ou_impar()
+  
